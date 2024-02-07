fa02fe19de8afc9898271cc95a7cb1559c68e24c
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-07 21:45:29 +0100

    ejercicio 4 especificamos los valores q le tenemos q pedir al usuario

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Introduzca la distancia recorrida (km): ");
+        double distanciaRecorrida = scanner.nextDouble();
+
+
+        System.out.print("Introduzca el tiempo empleado (horas): ");
+        double tiempoEmpleado = scanner.nextDouble();
+
+
+    }
 }
