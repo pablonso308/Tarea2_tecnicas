cb89fa11166ec362296666ca31275966c59191c0
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-07 17:16:19 +0100

    clases ejercicios

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,4 +1,19 @@
 //asumiendo que dentro del bucle se realizaran operaciones relazionas con el metodo de machine learning entendemos que el valor de a y b se modificara en cada iteracion del bucle
 //alor1 y valor2 podrían representar características (features):
+
 //Dentro del bucle, se podrían estar extrayendo o manipulando características específicas de los datos de entrada.
 // Estas características podrían ser utilizadas como variables predictoras en un modelo de aprendizaje automático.
+
+//cadena1 podría representar etiquetas (labels) o resultados del modelo:
+
+//La variable cadena1 podría contener información relacionada con las etiquetas o resultados generados por el modelo de machine learning.
+// Esto podría ser la salida predicha del modelo o algún otro tipo de información relevante.
+
+//Operaciones relacionadas con el entrenamiento o evaluación del modelo:
+//
+//Dentro del bucle, podrían estar ocurriendo operaciones que están relacionadas con el entrenamiento o evaluación del modelo.
+// Esto podría incluir procesos como la alimentación de datos al modelo, ajuste de parámetros, predicciones o evaluación del rendimiento del modelo
+
+//Iteración sobre datos de entrenamiento o conjunto de datos:
+//
+//El bucle "para" podría estar iterando sobre un conjunto de datos de entrenamiento, lo que indicaría que las operaciones dentro del bucle están destinadas a manipular y procesar datos de entrenamiento.
