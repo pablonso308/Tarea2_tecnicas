a7c5d2c7e81e1c7193106f642aa4e3801b71138d
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-07 16:28:41 +0100

    clases ejercicios

---
+++ Ejercicio1.java
@@ -0,0 +1,8 @@
+//En este código, la variable 'a' se inicializa con el valor 3, pero no se proporciona más información sobre cómo cambia a lo largo del algoritmo.
+// Sin información adicional, no podemos especular sobre cambios específicos en 'a' o 'b'.
+//En el contexto de un modelo predictivo, las variables 'a' y 'b' podrían representar características o atributos de los datos de entrada.
+// Estos valores podrían cambiar a medida que se procesan y transforman los datos a lo largo del flujo del modelo.
+//La transformación de datos es esencial en el campo de la inteligencia artificial.
+// Antes de alimentar los datos a un modelo, es común realizar diversas transformaciones para mejorar la calidad y relevancia de la información. Esto podría incluir normalización, escalamiento, codificación de variables categóricas, entre otras técnicas.
+//La visualización de datos también es crucial en la IA. Proporciona una comprensión intuitiva de las distribuciones, patrones y relaciones dentro de los datos.
+// La visualización puede ayudar a identificar posibles problemas en los datos, comprender la importancia relativa de diferentes características y validar la salida del modelo.
