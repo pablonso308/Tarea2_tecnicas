cc6fe50e512fe11f1273896cd2ed75f2adab5ee7
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-07 17:02:08 +0100

    clases ejercicios

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,2 +1,4 @@
-public class Ejercicio2 {
-}
+//asumiendo que dentro del bucle se realizaran operaciones relazionas con el metodo de machine learning entendemos que el valor de a y b se modificara en cada iteracion del bucle
+//alor1 y valor2 podrían representar características (features):
+//Dentro del bucle, se podrían estar extrayendo o manipulando características específicas de los datos de entrada.
+// Estas características podrían ser utilizadas como variables predictoras en un modelo de aprendizaje automático.
