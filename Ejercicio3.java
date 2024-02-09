b2bb8e130f750be8588db0b3067fc841d91a82bd
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-09 14:00:31 +0100

    ejercicio 3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -2,5 +2,5 @@
 // Los errores lógicos pueden llevar a resultados incorrectos, decisiones erróneas y, en última instancia, afectar negativamente la confiabilidad del modelo.
 //
 //La importancia de la precisión y corrección en la manipulación de datos para modelos de IA radica en la toma de decisiones basada en esos datos.
-// los modelos de IA aprenden patrones a partir de datos de entrenamiento y luego aplican esos patrones para hacer predicciones o tomar decisiones en nuevos datos.
+// Los modelos de IA aprenden patrones a partir de datos de entrenamiento y luego aplican esos patrones para hacer predicciones o tomar decisiones en nuevos datos.
 // Si hay errores en la manipulación de datos, estos patrones pueden ser incorrectos, lo que lleva a predicciones y decisiones erróneas.
