7600e836fe96f902e7b3357d23c1328d13b64bc8
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-09 13:50:59 +0100

    ejercicio 4

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,1 +1,6 @@
+//Si 'valor1' y 'valor2' son elementos cruciales en un modelo de IA y hay un error lógico en su manipulación, esto puede tener consecuencias significativas en la precisión y eficacia del modelo.
+// Los errores lógicos pueden llevar a resultados incorrectos, decisiones erróneas y, en última instancia, afectar negativamente la confiabilidad del modelo.
 //
+//La importancia de la precisión y corrección en la manipulación de datos para modelos de IA radica en la toma de decisiones basada en esos datos.
+// Los modelos de IA aprenden patrones a partir de datos de entrenamiento y luego aplican esos patrones para hacer predicciones o tomar decisiones en nuevos datos.
+// Si hay errores en la manipulación de datos, estos patrones pueden ser incorrectos, lo que lleva a predicciones y decisiones erróneas.
